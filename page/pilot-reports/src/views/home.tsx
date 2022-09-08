5862f6ffab005316147440a8ce96f00154a77b22
Harvey Bates <phenobottle@gmail.com>
2022-09-09 08:35:19 +1000

    started on oyster farming regions frontend

---
+++ page/pilot-reports/src/views/home.tsx
@@ -0,0 +1,13 @@
+import React, {useRef} from 'react';
+import {Map} from "../components/map";
+
+export default class Home extends React.Component<any, any>{
+
+    render(){
+        return (
+            <div>
+                <Map/>
+            </div>
+        )
+    }
+}
